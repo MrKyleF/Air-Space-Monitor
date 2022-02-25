047b58ea4b6067271feefb438e7ca02c0d6ebc33
root <admin@example.com>
2022-02-25 15:46:32 -0500

    Recent Update To Flight Logs

--- FlightLog.py
+++ FlightLog.py
@@ -2,17 +2,21 @@
 #Flight Log: Contains Past Data of Aircraft
 
 from LogEntry import LogEntry
+from FlightLogHelper import *
 from FlightRadar24.api import FlightRadar24API
 
 
 class FlightLog:
     def setDefaultInfomation(self):
-        print("")
+        self.airline_name, self.callsign, self.country_flag, self.comments, self.full_name = getAirline(iata=self.flight.airline_iata, icao=self.flight.airline_icao)
+        self.aircraft_name = getAircraftName(aircraft_code=self.flight.aircraft_code)
+
     #Create Log Entry For Each Query
     def createLogEntry(self, flight):
         self.logs.append(LogEntry(flight))
 
     def __init__(self, flight):
-        self.flight = flight
-        self.logs = []
-        self.createLogEntry(flight)
+        self.flight = flight            #Flight Radar Flight Object
+        self.logs = []                  #Logs Of This Aircraft from Queries
+        self.createLogEntry(flight)     #Creates First Logs
+        self.setDefaultInfomation()     #Sets All Defualt Information From Local Files
--- FlightLogHelper.py
+++ FlightLogHelper.py
@@ -9,12 +9,12 @@ def getAirline(iata="N/A", icao="N/A"):
         csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in csv_reader:
             if (row[0] == iata and row[0] != "N/A" ) or (row[1] == icao and row[1] != "N/A"):
-                airline_name = row[2] if row[2] != '' else "N/A"
-                callsign = row[3] if row[3] != '' else "N/A"
-                country_flag = row[4] if row[4] != '' else "N/A"
-                comments = row[5] if row[5] != '' else "N/A"
-                full_name = str(airline_name + ": ") if airline_name != "N/A" else ""
-                full_name += (comments) if comments != "N/A" else ""
+                airline_name = row[2] if row[2] != '' else "N/A"                        #Airline Name From Database File
+                callsign = row[3] if row[3] != '' else "N/A"                            #Call Sign From Database File
+                country_flag = row[4] if row[4] != '' else "N/A"                        #Country Flag From Databse File
+                comments = row[5] if row[5] != '' else "N/A"                            #Comments On Airline From Database File
+                full_name = str(airline_name + ": ") if airline_name != "N/A" else ""   
+                full_name += (comments) if comments != "N/A" else ""                    #Constructed Full Name For Airline
                 return airline_name, callsign, country_flag, comments, full_name
     
     return None, None, None, None, None
@@ -25,11 +25,11 @@ def getAircraftName(aircraft_code = "N/A", skip_second_check=False):
         #Check Row 1 For Aircraft Code: Shows All Options
         for row in csv_reader:
             if (row[1] == aircraft_code and row[1] != "N/A"):
-                return row[2] if row[2] != '' else "N/A"        #Return Aircraft Name
+                return row[2] if row[2] != '' else "N/A"        #Return Aircraft Name From Database File
         #Check Row 0
         if skip_second_check == False:                          #Can be skipped if values are all being found in check 0
             for row in csv_reader:
                 if (row[0] == aircraft_code and row[0] != "N/A" ):
-                    return row[2] if row[2] != '' else "N/A"    #Return Aircraft Name
+                    return row[2] if row[2] != '' else "N/A"    #Return Aircraft Name From Database File
     
-    return "Raw: " + aircraft_code                              #Not Found Return it with Raw Prefix
+    return "Raw: " + aircraft_code                              #Aircraft Not Found In Database File, Return Input With Raw Prefix
